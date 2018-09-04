c6e75e1a0653b3f642d20648d18fee6c9fc13416
shuidian <185360991@qq.com>
2018-09-04 22:43:21 +0800

    同步代码

---
+++ src/main/java/com/gmall/service/ICategoryService.java
@@ -0,0 +1,7 @@
+package com.gmall.service;
+
+import com.gmall.common.ServerResponse;
+
+public interface ICategoryService {
+    ServerResponse addCategory(String categoryName, Integer parentId);
+}
