b2a4c00128be234cf4da1998d2bd525cb0c510d9
jiangyan <yan.jiang@kindroid.com>
2018-09-04 22:36:27 +0800

    同步代码

--- src/main/java/com/gmall/common/Const.java
+++ src/main/java/com/gmall/common/Const.java
@@ -2,4 +2,12 @@ package com.gmall.common;
 
 public class Const {
     public static final String CURRENT_USER = "currentUser";
+
+    public static final String EMAIL = "email";
+    public static final String USERNAME = "username";
+
+    public interface Role{
+        int Role_CUSTOMER = 0; // 普通用户
+        int Role_ADMIN = 1; // 管理员
+    }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,6 @@
 <html>
 <body>
 <h2>Hello World!123</h2>
+<script src="http://libs.baidu.com/jquery/2.0.0/jquery.min.js"></script>
 </body>
 </html>
